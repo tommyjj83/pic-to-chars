0f75d15e501933eacd5908929404098d86e46068
Tomáš Jaroš <tomasek.jaros@seznam.cz>
2025-09-27 21:53:44 -0400

    Input validation changed.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        if (validateArgs(args)) {
+        int[] output_quality = {0};
+        String[] file = {""};
+        if (validateArgs(args, file, output_quality)) {
             return;
         }
     }
 
 
-    private static boolean validateArgs(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Wrong number of arguments. Usage: app path-to-image");
+    private static boolean validateArgs(String[] args, String[] file, int[] outputQuality) {
+        if (args.length != 2) {
+            System.err.println("Wrong number of arguments. Usage: app path-to-image output_quality");
+            return false;
+        }
+
+        file[0] = args[0];
+
+        try {
+             outputQuality[0] = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.err.println("output-quality must be an integer");
             return false;
         }
 
