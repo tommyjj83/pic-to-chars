2b71a58e78f05138238cdaf272f1dbbae030e5ae
Tomáš Jaroš <tomasek.jaros@seznam.cz>
2025-09-27 21:37:41 -0400

    Arguments validation added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        if (validateArgs(args)) {
+            return;
+        }
+    }
+
+
+    private static boolean validateArgs(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Wrong number of arguments. Usage: app path-to-image");
+            return false;
+        }
+
+        return true;
     }
 }
